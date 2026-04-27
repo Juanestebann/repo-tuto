92afed9c2629f1e64d0265eb5b4475d554e0b62a
Juanestebann <juanestebann14@gmail.com>
2026-04-26 21:00:28 -0400

    Prueba 2

--- logistica/logistica/src/main/java/com/example/logistica/LogisticaApplication.java
+++ logistica/logistica/src/main/java/com/example/logistica/LogisticaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogisticaApplication {
-
+//Pruebaaaaa
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticaApplication.class, args);
 	}
